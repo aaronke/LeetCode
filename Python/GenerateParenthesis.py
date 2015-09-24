d1c004246cac6275544ee9c98173cdce06b187e2
Kai Zhou <kai.zhou2013@gmail.com>
2015-09-24 15:15:12 -0600

    Update GenerateParenthesis.py

--- Python/GenerateParenthesis.py
+++ Python/GenerateParenthesis.py
@@ -1,3 +1,26 @@
+# recursive DP
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        # dp, f(n) = '(f(0))'+ f(n-1)
+        #           + '(f(1))' + f(n-2)
+        #           + '(f(2))' + f(n-3)
+        #           + '(f(n-1))' + f(0)
+        if n == 0:
+            return ['']
+        result = []
+        for i in range(n):
+            left = self.generateParenthesis(i)
+            right = self.generateParenthesis(n-1-i)
+            for left_i in left:
+                for right_i in right:
+                    result.append('(' + left_i + ')' + right_i)
+        return result
+
+# store DP results in 2d list        
 class Solution(object):
     def generateParenthesis(self, n):
         """
