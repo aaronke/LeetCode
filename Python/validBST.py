0226ce8c5c118b106bf09f3eb50fb218427b6b4e
Kai Zhou <kai.zhou2013@gmail.com>
2015-10-08 16:39:05 -0600

    Update validBST.py

--- Python/validBST.py
+++ Python/validBST.py
@@ -1,4 +1,63 @@
-class Solution(object):
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# iterative check in-order traversal, most efficient, O(N)
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if root == None:
+            return True
+        stack = []
+        stack.append(root)
+        while root.left != None:
+            root = root.left
+            stack.append(root)
+        import sys
+        prev = -sys.maxint-1
+        while len(stack) != 0:
+            node = stack.pop()
+            if node.val <= prev:
+                return False
+            else:
+                prev = node.val
+            if node.right != None:
+                node = node.right
+                stack.append(node)
+                while node.left != None:
+                    node = node.left
+                    stack.append(node)
+        return True
+# recursive in-order
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if root == None:
+            return True
+        import sys
+        self.prev = -sys.maxint-1
+        self.flag = True
+        self.inorder(root)
+        return self.flag
+    
+    def inorder(self, root):
+        if root == None or self.flag == False:
+            return
+        self.inorder(root.left)
+        if root.val <= self.prev:
+            self.flag = False
+        else:
+            self.prev = root.val
+        self.inorder(root.right)
+
+# check root.val with left subtree max, and right sub-tree min
     def isValidBST(self, root):
         """
         :type root: TreeNode
@@ -19,8 +78,10 @@ class Solution(object):
             if runner.val <= root.val:
                 return False
         return self.isValidBST(root.left) and self.isValidBST(root.right)
-import sys
-class Solution(object):
+        
+
+# recursive check max, min
+    import sys
     def isValidBST(self, root):
         """
         :type root: TreeNode
