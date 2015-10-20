b153674692babe6e64ebe5022f4cdca54eaccae6
Kai Zhou <kai.zhou2013@gmail.com>
2015-10-20 12:06:30 -0600

    Update NumOfIslands.py

--- Python/NumOfIslands.py
+++ Python/NumOfIslands.py
@@ -22,3 +22,28 @@ class Solution:
         self.dfs(grid,x+1,y)
         self.dfs(grid,x,y-1)
         self.dfs(grid,x,y+1)
+
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        if len(grid) < 1 or len(grid[0]) < 1:
+            return 0
+        result = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    result += 1
+                    self.dfs(grid, i, j)
+        return result
+        
+    def dfs(self, grid, i, j):
+        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != '1':
+            return
+        grid[i][j] = '#'
+        self.dfs(grid, i+1, j)
+        self.dfs(grid, i-1, j)
+        self.dfs(grid, i, j+1)
+        self.dfs(grid, i, j-1)
