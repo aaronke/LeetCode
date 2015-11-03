26ec52f6b0803963216cd14e0e80b7d75644cab9
Kai Zhou <kai.zhou2013@gmail.com>
2015-11-03 14:12:47 -0700

    Create MoveZeros.py

---
+++ Python/MoveZeros.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        if nums is None or len(nums) < 2:
+            return
+        slow, fast = 0, 0
+        while fast < len(nums):
+            if nums[fast] != 0:
+                nums[slow] = nums[fast]
+                slow += 1
+            fast += 1
+        while slow < len(nums):
+            nums[slow] = 0
+            slow += 1
